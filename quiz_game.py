3c37f25a8d94309a9beaa30b0d91d0ca1a0ad69b
brianjo94 <brianjo1994@gmail.com>
2023-02-13 21:54:40 -0500

    quiz_game

---
+++ quiz_game.py
@@ -0,0 +1,39 @@
+print('Welcome to my computer quiz')
+
+playing = input('Do you want to play? ')
+
+if playing.lower() != 'yes':
+    quit()
+
+print("Okay Let's Go!")
+score = 0
+
+answer = input("What does CPU stand for?: ")
+if answer.lower() == "central processing unit":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does GPU stand for?: ")
+if answer.lower() == "graphics processing unit":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does RAM stand for?: ")
+if answer.lower() == "random access memory":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does PSU stand for?: ")
+if answer.lower() == "power supply":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+print(f'you scored total of {score/4 * 100}% score')
