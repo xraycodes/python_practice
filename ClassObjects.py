e8f8f0d6711da9f7ce5397c5040894e19a9354ca
brianjo94 <brianjo1994@gmail.com>
2023-02-20 20:46:01 -0500

    Examples using class attributes and classmethods

--- ClassObjects.py
+++ ClassObjects.py
@@ -1,43 +1,20 @@
-class Pet:
-    def __init__(self, name, age):
+
+class Person:
+    number_of_people = 0
+
+    def __init__(self, name):
         self.name = name
-        self.age = age
+        Person.add_person()
+        
+    @classmethod    
+    def add_person(cls):
+        cls.number_of_people += 1
 
-    def show(self):
-        print(f"I am {self.name} and I am {self.age} years old")
+    @classmethod
+    def number_of_people_(cls):
+        return cls.number_of_people
 
-    def speak(self):
-        print("I dont know what I say")
+p1 = Person("Tim")
+p1 = Person("Jill")
 
-class Cat(Pet):
-    def __init__(self, name, age, color):
-        super().__init__(name,age)
-        self.color = color
-
-    def show(self):
-        print(f"I am {self.name} and I am {self.age} years old and am {self.color}")
-
-    def speak(self):
-        print('meow')
-
-class Dog(Pet):
-    def speak(self):
-        print('bark')
-
-class Fish(Pet):
-    pass
-
-
-p = Pet('Tim', 19)
-p.show()
-
-c = Cat("Bill", 34, 'pink')
-c.show()
-c.speak()
-
-d = Dog("Jill", 25)
-d.show()
-d.speak()
-
-f = Fish('Bubbles', 10)
-f.speak()
+print(Person.number_of_people_())
