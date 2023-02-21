c82277113cf1d58b9507a1b926448d658dbbc672
brianjo94 <brianjo1994@gmail.com>
2023-02-20 20:32:37 -0500

    Class objects using inheritance

--- ClassObjects.py
+++ ClassObjects.py
@@ -1,39 +1,43 @@
-
-
-class Student:
-    def __init__(self, name, age, grade):
+class Pet:
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.grade = grade   #0-100
 
-    def get_grade(self):
-        return self.grade       
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old")
 
-class Course:
-    def __init__(self, name, max_students):
-        self.name = name
-        self.max_students = max_students
-        self.students = []
+    def speak(self):
+        print("I dont know what I say")
 
-    def add_students(self, student):
-        if len(self.students) < self.max_students:
-            self.students.append(student)
-            return True
-        return False
+class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name,age)
+        self.color = color
 
-    def get_average_grade(self):
-        value = 0
-        for student in self.students:
-            value += student.get_grade()
-        return value / len(self.students)
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old and am {self.color}")
 
-s1 = Student('Tim', 19, 95)
-s2 = Student('Bill', 19, 75)
-s3 = Student('Jill', 19, 65)
+    def speak(self):
+        print('meow')
 
-course = Course('Science', 2)
-course.add_students(s1)
-course.add_students(s2)
+class Dog(Pet):
+    def speak(self):
+        print('bark')
 
-print(course.students)
-print(course.get_average_grade())
+class Fish(Pet):
+    pass
+
+
+p = Pet('Tim', 19)
+p.show()
+
+c = Cat("Bill", 34, 'pink')
+c.show()
+c.speak()
+
+d = Dog("Jill", 25)
+d.show()
+d.speak()
+
+f = Fish('Bubbles', 10)
+f.speak()
