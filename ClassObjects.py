5a9a774a03330821e0e15f12df0816f70019f069
brianjo94 <brianjo1994@gmail.com>
2023-02-20 19:12:38 -0500

    Class Dog object example

---
+++ ClassObjects.py
@@ -0,0 +1,11 @@
+class Dog:
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+
+    def dog_name(self):
+        print(self.name)
+
+d = Dog("cash")
+d.dog_name()
+
