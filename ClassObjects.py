b5cb04ad76e6398bac992460f213fe59a602d48c
brianjo94 <brianjo1994@gmail.com>
2023-02-20 21:05:44 -0500

    Static methods

--- ClassObjects.py
+++ ClassObjects.py
@@ -1,20 +1,7 @@
+class Math:
+    
+    @staticmethod
+    def add5(x):
+        return x + 5
 
-class Person:
-    number_of_people = 0
-
-    def __init__(self, name):
-        self.name = name
-        Person.add_person()
-        
-    @classmethod    
-    def add_person(cls):
-        cls.number_of_people += 1
-
-    @classmethod
-    def number_of_people_(cls):
-        return cls.number_of_people
-
-p1 = Person("Tim")
-p1 = Person("Jill")
-
-print(Person.number_of_people_())
+print(Math.add5(5))
