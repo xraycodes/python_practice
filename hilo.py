6b77b4ba0d53a7507c65fa1b2f335d30d2a3bef7
brianjo94 <brianjo1994@gmail.com>
2023-07-02 17:36:03 -0400

    Added new functions

--- hilo.py
+++ hilo.py
@@ -32,7 +32,16 @@ def guess_binary(answer, low, high):
         guesses += 1
 
 
+correct_count = 0
+max_guesses = 0
 for number in range(LOW, HIGH + 1):
     number_of_guesses = guess_binary(number, LOW, HIGH)
     print("{} guessed in {}".format(number, number_of_guesses))
+
+    if number_of_guesses > max_guesses:
+        max_guesses, correct_count = number_of_guesses, 1
+    elif number_of_guesses == max_guesses:
+        correct_count += 1
+print("I guessed without being told {} times. Max {} guesses."
+      .format(correct_count, max_guesses))
 
