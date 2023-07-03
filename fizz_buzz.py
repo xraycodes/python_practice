faa773e940a8d199384f4bc34dc5f0aa9e9ebb51
brianjo94 <brianjo1994@gmail.com>
2023-07-03 12:11:05 -0400

    Updated game to have player interaction

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -21,7 +21,19 @@ def fizz_buzz(number: int) -> str:
         return str(number)
         
 
-number_request = int(input("What number do you want to play until? "))
-for i in (range(1,number_request + 1)):
-    print(fizz_buzz(i))
+input("Fizzbuzz.. Press enter to start ")
+print()
+
+next_number = 0
+while next_number < 99:
+    next_number += 1
+    print(fizz_buzz(next_number))
+    next_number += 1
+    correct_answer = fizz_buzz(next_number)
+    players_answer = input("Your go: ")
+    if players_answer != correct_answer:
+        print("You lose, the correct answer was {}".format(correct_answer))
+        break   
+else:
+    print("Well done you reached {}".format(next_number))
 
