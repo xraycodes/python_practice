671241c9e5b8fbb2438dc98d87d19eff3517121e
brianjo94 <brianjo1994@gmail.com>
2023-07-04 15:50:54 -0400

    Updated using setdefault method

--- words_in_text.py
+++ words_in_text.py
@@ -6,13 +6,9 @@ text = "Later in the course, you'll see how to use the collections Counter class
  
 # Your code goes here ...
 
-for letter in text:
-    lowercase = letter.casefold()
-    if lowercase.isalnum():
-        if lowercase in word_count:
-            word_count[lowercase] += 1
-        else:
-            word_count[lowercase] = 1
+for letter in text.casefold():
+    if letter.isalnum():
+        word_count.setdefault(letter, 1) + 1
  
 # Printing the dictionary
 for letter, count in sorted(word_count.items()):
