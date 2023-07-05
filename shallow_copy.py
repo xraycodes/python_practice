e1ba210c7708e93f6ac56ac63396d1b352b70f50
brianjo94 <brianjo1994@gmail.com>
2023-07-05 18:07:44 -0400

    Using deepcopy function on dictionaries

--- shallow_copy.py
+++ shallow_copy.py
@@ -1,10 +1,15 @@
+import copy
+
 animals = {
     "lion": ["scary", "big", 'cat'],
     "elephant": ["big", 'grey','wrinkled'],
     "teddy": ["cuddly", "stuffed"]
 }
+#Perform a shallow copy
+#things = animals.copy()
 
-things = animals.copy()
+#Perform a deep copy
+things = copy.deepcopy(animals)
 
 print(things["teddy"])
 print(animals["teddy"]) 
