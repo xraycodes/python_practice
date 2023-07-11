7ef4f95cb4b27593df803902b68ea0b7e8d2f412
brianjo94 <brianjo1994@gmail.com>
2023-07-10 22:41:27 -0400

    Practicing random dictionary methods and uses

---
+++ scrap_notebook.py
@@ -0,0 +1,18 @@
+fridge = {
+    "apple" : 5,
+    "beef": 2,
+    "carrots": 3,
+    "ketchup": 5,
+    
+}
+
+f = {
+    "beef": 3,
+    "carrots": 2,
+}
+
+r = fridge.values()
+print(r)
+
+if 5 in r:
+    print('yo')
