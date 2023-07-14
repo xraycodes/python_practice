7063ae5028f4a10817321a4ade8e88b324960f54
brianjo94 <brianjo1994@gmail.com>
2023-07-14 19:36:16 -0400

    filtering list into set and removing duplicate items

--- dictionary/set_intro.py
+++ dictionary/set_intro.py
@@ -1,6 +1,11 @@
 numbers = set()
 
-while len(numbers) < 4: 
-    next_value = int(input("Type number to enter: "))
-    numbers.add(next_value)
-print(numbers)
+# while len(numbers) < 4: 
+#     next_value = int(input("Type number to enter: "))
+#     numbers.add(next_value)
+# print(numbers)
+
+data = ["blue", "red", "blue", "green", "red", "blue", "white"]
+# Create a set from the list, to remove duplicates
+unique_data = set(data)
+print(unique_data)
