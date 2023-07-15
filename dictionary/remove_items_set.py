ac59b9353126ace04003d328f610a8b932506c94
brianjo94 <brianjo1994@gmail.com>
2023-07-14 20:51:21 -0400

    Using examples of why discard preferred over remove

---
+++ dictionary/remove_items_set.py
@@ -0,0 +1,67 @@
+# small_ints = set(range(21))
+# print(small_ints)
+
+# small_ints.discard(10)
+# small_ints.remove(11)
+# print(small_ints)
+
+# small_ints.remove(99)
+# print(small_ints)
+
+travel_mode = {"1": "car", "2": "plane"}
+
+items = {
+    "can opener",
+    "fuel",
+    "jumper",
+    "knife",
+    "matches",
+    "razor blades",
+    "razor",
+    "scissors",
+    "shampoo",
+    "shaving cream",
+    "shirts (3)",
+    "shorts",
+    "sleeping bag(s)",
+    "soap",
+    "socks (3 pairs)",
+    "stove",
+    "tent",
+    "mug",
+    "toothbrush",
+    "toothpaste",
+    "towel",
+    "underwear (3 pairs)",
+    "water carrier",
+}
+
+restricted_items = {
+    "catapult",
+    "fuel",
+    "gun",
+    "knife",
+    "razor blades",
+    "scissors",
+    "shampoo",
+}
+
+print("Please choose your mode of travel:")
+for key, value in travel_mode.items():
+    print(f"{key}: {value}")
+    # Python 3.5 and earlier
+    # print("{}: {}".format(key, value))
+
+mode = "-"
+while mode not in travel_mode:
+    mode = input("> ")
+
+if mode == "2":
+    # travelling by plane, remove restricted items
+    for restricted_item in restricted_items:
+        items.discard(restricted_item)
+
+# print the packing list
+print("You need to pack:")
+for item in sorted(items):
+    print(item)
