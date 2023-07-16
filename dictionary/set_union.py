b88a8ba3415f9cdd40508c6baa27b0323a9b454d
brianjo94 <brianjo1994@gmail.com>
2023-07-15 21:28:37 -0400

    set_union using update and args parameter

--- dictionary/set_union.py
+++ dictionary/set_union.py
@@ -1,8 +1,20 @@
-farm_animals = {"sheep", "hen", "cow", "horse", "goat"}
-wild_animals = {"lion", "elephant", "tiger", "goat", "panther", "horse"}
+# farm_animals = {"sheep", "hen", "cow", "horse", "goat"}
+# wild_animals = {"lion", "elephant", "tiger", "goat", "panther", "horse"}
 
-all_animals_1 = farm_animals.union(wild_animals)
-print(all_animals_1)
+# all_animals_1 = farm_animals.union(wild_animals)
+# print(all_animals_1)
 
-all_animals_2 = wild_animals | farm_animals
-print(all_animals_2)
+# all_animals_2 = wild_animals | farm_animals
+# print(all_animals_2)
+
+from prescription_data import adverse_interactions
+
+meds_to_watch = set()
+
+# for interaction in adverse_interactions:
+#     # meds_to_watch = meds_to_watch.union(interaction)
+#     meds_to_watch.update(interaction)
+
+meds_to_watch.update(*adverse_interactions)
+
+print(sorted(meds_to_watch))
