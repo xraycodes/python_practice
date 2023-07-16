679fc422ca13729afe8cdc4ed3bec614b1bbcfc8
brianjo94 <brianjo1994@gmail.com>
2023-07-15 20:56:32 -0400

    Example of set union

---
+++ dictionary/set_union.py
@@ -0,0 +1,8 @@
+farm_animals = {"sheep", "hen", "cow", "horse", "goat"}
+wild_animals = {"lion", "elephant", "tiger", "goat", "panther", "horse"}
+
+all_animals_1 = farm_animals.union(wild_animals)
+print(all_animals_1)
+
+all_animals_2 = wild_animals | farm_animals
+print(all_animals_2)
