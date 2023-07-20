2a5929f4c927eecfa91783310b230ef1b64bb9ac
brianjo94 <brianjo1994@gmail.com>
2023-07-19 22:23:38 -0400

    Practicing working with sets

--- scrap_notebook.py
+++ scrap_notebook.py
@@ -1,18 +1,6 @@
-fridge = {
-    "apple" : 5,
-    "beef": 2,
-    "carrots": 3,
-    "ketchup": 5,
-    
-}
+se = {'hi','bye','do'}
+pe = {'hi', 'do', 'bye'}
 
-f = {
-    "beef": 3,
-    "carrots": 2,
-}
+numbers =  {'hi','bye','do'}
 
-r = fridge.values()
-print(r)
-
-if 5 in r:
-    print('yo')
+print(numbers.pop())
