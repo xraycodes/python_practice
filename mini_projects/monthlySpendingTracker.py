ca9af6c2fe7bf97db9b3de6b391ba2acd4b8a2ff
brianjo94 <brianjo1994@gmail.com>
2023-07-21 13:17:27 -0400

    Comments explaining what program does. Bare bone

---
+++ mini_projects/monthlySpendingTracker.py
@@ -0,0 +1,21 @@
+# Program designed to keep track of monthly expenses
+# Calculates Cash Flow (Money going out, in, net)
+
+# Database
+    # Monthly spending. Dictionary(Key: Month, Value: Spendings)
+
+#Client side
+    # Option to 1) enter items for the month or 2)to look through previous months
+    # Output 1: Which items to enter for that month
+    # Output 2: Which previous month user requests
+
+    # Add up total for liability
+    # Add up total for asset/monthly income
+    # Count for net 
+
+# Output Code
+user_option = input(
+    """Choose option:
+1) For current month 
+2) See previous months
+:""")
