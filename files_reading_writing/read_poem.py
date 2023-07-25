0cd9daf2a61cbb5528dc1240d1b2a40010e4de4a
brianjo94 <brianjo1994@gmail.com>
2023-07-24 20:47:48 -0400

    ways to read and open files

--- files_reading_writing/read_poem.py
+++ files_reading_writing/read_poem.py
@@ -1,11 +1,39 @@
-# jabber = open('Jabberwocky.txt', 'r')
+# # jabber = open('Jabberwocky.txt', 'r')
+# #
+# # for line in jabber:
+# #     print(line.rstrip())
+# #     # print(len(line))
+# #
+# # jabber.close()
+#
+# with open('Jabberwocky.txt', 'r') as jabber:
+#     # for line in jabber:
+#     #     print(line.rstrip())
+#     lines = jabber.readlines()
+#
+# print(lines)
+# print(lines[-1:])
+# for line in reversed(lines):
+#     print(line, end='')  # do some processing in reverse order
+#
+# with open('Jabberwocky.txt') as jabber:
+#     text = jabber.read()
+#
+# # print(text)
+# for character in reversed(text):
+#     print(character, end='')
 
-# for line in jabber:
-#     print(line)
-#     # print(line.strip())
-#     # print(len(line))
-# jabber.close()
+with open('Jabberwocky.txt') as jabber:
+    while True:
+        line = jabber.readline().rstrip()
+        print(line)
+        if 'jubjub' in line.casefold():
+            break
 
-with open('Jabberwocky.txt', 'r') as jabber:
+print('*' * 80)
+
+with open('Jabberwocky.txt') as jabber:
     for line in jabber:
         print(line.rstrip())
+        if 'jubjub' in line.casefold():
+            break
