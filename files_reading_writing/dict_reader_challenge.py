f39537576b7818237216f6829974dafa1dc3034d
brianjo94 <brianjo1994@gmail.com>
2023-08-04 21:57:47 -0400

    Using dialect/fieldnames to modify headings for DictReader

--- files_reading_writing/dict_reader_challenge.py
+++ files_reading_writing/dict_reader_challenge.py
@@ -2,24 +2,31 @@ import csv
 
 input_filename = 'country_info.txt'
 
+dialect = csv.excel
+dialect.delimiter = '|'
+
 countries = {}
 with open(input_filename, encoding='utf-8', newline='') as country_file:
-    dict_reader = csv.DictReader(country_file, delimiter = '|')
+    # Get the column headings from the first line of file
+    headings = country_file.readline().strip('\n').split(dialect.delimiter)
+    for index, heading in enumerate(headings):
+        headings[index] = heading.casefold()
+
+    dict_reader = csv.DictReader(country_file, dialect=dialect, fieldnames=headings)
     for row in dict_reader:
         # countries[country.casefold()] = country_dict
-        countries[row['Country'].casefold()] = row
+        countries[row['country'].casefold()] = row
         # code_lookup[code.casefold()] = country
-        countries[row['CC'].casefold()] = row
-
- 
+        countries[row['cc'].casefold()] = row
+        
 while True:
     chosen_country = input("What country would you like to know? ").casefold()
     if chosen_country in countries:
         country_data = countries[chosen_country]
-        if country_data['Capital'] == '':
+        if country_data['capital'] == '':
             print(f"{chosen_country} does not have a capital city")
         else:
-            print(f"The capital of {chosen_country} is {country_data['Capital']}")
+            print(f"The capital of {chosen_country} is {country_data['capital']}")
     elif chosen_country == 'quit':
         break
     else:
