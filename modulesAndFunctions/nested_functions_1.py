1a38e02e44ea021a099b680d1573e7dda34c1d3f
brianjo94 <brianjo1994@gmail.com>
2023-08-15 13:07:21 -0400

    Reassigning free variables and its effect

--- modulesAndFunctions/nested_functions_1.py
+++ modulesAndFunctions/nested_functions_1.py
@@ -2,10 +2,13 @@ def greet_pythons(items: list) -> None:
     greeting = 'Hello'
 
     def make_greeting(item: str) -> str:
+        # greeting = 'Hi'
         return f'{greeting} {item}'
+        
 
     for item in items:
         print(make_greeting(item))
+    print(f'Inside greet_pythons,`greeting` is {greeting}')
 
 
 names = ['John', 'Eric', 'Graham', 'Terry', 'Terry', 'Michael']
