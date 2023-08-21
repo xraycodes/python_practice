8ab59b12e390c559580ffb7295e8f2c64d6ec826
brianjo94 <brianjo1994@gmail.com>
2023-08-20 22:56:24 -0400

    updated more codes, using nested dict

--- mini_projects/chipotle_mobile_order.py
+++ mini_projects/chipotle_mobile_order.py
@@ -2,19 +2,37 @@
 
 #Database
 TAX = 0.13
-    #Lists of food criteria(Protein, Rice, Beans, Sauce, Extra
-extra = ['Veggies', 'Lettuce', 'Cheese', 'Corn']
-sauce = ['Mild', 'Medium', 'Hot', 'Queso']
 user_order = []
-
-protein = {'Chicken' : 7, 'Steak' : 8, 'Barbacoa': 7, 'Sofrita': 7}
-rice = {'Brown rice' : 3, 'White rice' : 3}
-beans = {'Pinto' : 1, 'Black' : 1}
-# extra = {'Veggies': 0, 'Lettuce'}
-
-
-    #Dictionary(Key=User_name, Value = Chosen order as a list)
+chosen_food_list = []
+#Dictionary(Key=User_name, Value = Chosen order as a list)
 user_name_order = {}
+#Lists of food criteria(Protein, Rice, Beans, Sauce, Extra
+food = {'Protein': {
+            'Chicken' : 7,
+            'Steak' : 8,
+                    },
+        'Rice': {
+            'Brown rice' : 3,
+            'White rice' : 3
+                },
+        'Beans': {
+            'Pinto' : 1,
+            'Black' : 1,
+                },
+        'Extra': {
+            'Veggies' : 0,
+            'Lettuce' : 0,
+            'Cheese' : 0,
+            'Corn' : 0,
+                },
+        'Sauce': {
+            'Mild' : 0,
+            'Medium' : 0,
+            'Hot' : 0,
+            'Queso' : 0,
+        }
+    }
+
 
 #Functions
 def food_iteration(data):
@@ -32,44 +50,40 @@ def item_price():
 
 # Main Code
 user_name = input("Who is this order for?: ")
+user_name_order[user_name] = 0
 
-food_iteration(protein)
-protein_choice = int(input("Choose your protein. "))
-user_order.append(protein[protein_choice - 1])
-print(f"Adding {protein[protein_choice - 1]}\n")
+while True:
+    for index, (key, value) in enumerate(food['Protein'].items()):
+        print(index + 1, key)
+    proteins_list = list(food['Protein'].keys())
+    user = int(input("What's your choice of protein?(In Numbers): "))
+    if 1 <= user <= len(food['Protein']):
+        chosen_protein = proteins_list[user - 1]
+        user_order.append(chosen_protein)
+        user_name_order[chosen_protein] = {'price' : food['Protein'][chosen_protein]}
+    else:
+        print('Number out of range, Try again')
 
-food_iteration(rice)
-rice_choice = int(input("Choose your rice."))
-user_order.append(rice[rice_choice - 1])
-print(f"Adding {rice[rice_choice- 1]}\n")
+    for index, (key, value) in enumerate(food['Rice'].items()):
+        print(index + 1, key)
+    rice_list = list(food['Rice'].keys())
+    user = int(input("What's your choice of rice?(In Numbers): "))
+    if 1 <= user <= len(food['Rice']):
+        chosen_rice = rice_list[user - 1]
+        user_order.append(chosen_rice)
+        user_name_order[chosen_rice] = {'price': food['Rice'][chosen_rice]}
+        break
+    else:
+        print('Number out of range, Try again')
+        break
+
+print(user_name_order)
 
 
-food_iteration(beans)
-beans_choice = int(input("Choose your beans. "))
-user_order.append(beans[beans_choice - 1])
-print(f"Adding {beans[beans_choice - 1]}\n")
-
-food_iteration(extra)
-extra_choice = int(input("Choose your extras."))
-user_order.append(extra[extra_choice - 1])
-print(f"Adding {extra[extra_choice - 1]}\n")
-
-food_iteration(sauce)
-sauce_choice = int(input("Choose your sauce. "))
-user_order.append(sauce[sauce_choice - 1])
-print(f"Adding {sauce[sauce_choice - 1]}\n")
-
-user_name_order[user_name] = user_order
-
-
-new = ','.join(user_order)
-print(f"Order for {user_name} is {new}")
-
-for i in user_order:
-    if i in proteins:
-        print(proteins[i])
-    elif i in rices:
-        print(rices[i])
+# food_iteration(protein)
+# protein_choice = int(input("Choose your protein. "))
+# user_order.append(protein[protein_choice - 1])
+# print(f"Adding {protein[protein_choice - 1]}\n")
 
 
 
