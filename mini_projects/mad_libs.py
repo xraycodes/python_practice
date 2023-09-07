8e809e260c33e5646f740e127392a12d77c33684
brianjo94 <brianjo1994@gmail.com>
2023-09-07 07:30:54 -0400

    math questions timed project

--- mini_projects/mad_libs.py
+++ mini_projects/mad_libs.py
@@ -3,49 +3,49 @@ text = 'story.txt'
 with open(text,) as f:
     story = f.read()
 
-words = set()
-start_of_word = -1
+# words = set()
+# start_of_word = -1
 
-target_start = "<"
-target_end = ">"
+# target_start = "<"
+# target_end = ">"
 
-for index, char in enumerate(story):
-    if char == target_start:
-        start_of_word = index
+# for index, char in enumerate(story):
+#     if char == target_start:
+#         start_of_word = index
     
-    if char == target_end and start_of_word != -1:
-        word = story[start_of_word: index + 1]
-        words.add(word)
-        start_of_word = -1
+#     if char == target_end and start_of_word != -1:
+#         word = story[start_of_word: index + 1]
+#         words.add(word)
+#         start_of_word = -1
 
-answers = {}
+# answers = {}
 
-for word in words:
-    answer = input(f"Enter a word for {word}: ")
-    answers[word] = answer
+# for word in words:
+#     answer = input(f"Enter a word for {word}: ")
+#     answers[word] = answer
 
-for word in words:
-    story = story.replace(word, answers[word])
+# for word in words:
+#     story = story.replace(word, answers[word])
 
-print(story)
+# print(story)
     
 
 
 
 
 
-# story_list = story.split()
-# replacement_words = []
+story_list = story.split()
+replacement_words = []
 
-# for word in story_list:
-#     if word.startswith('<'):
-#         replacement_words.append(word)
+for word in story_list:
+    if word.startswith('<'):
+        replacement_words.append(word)
 
-# for word in story_list:
-#     print(word, end = ' ')
-#     if word in replacement_words:
-#         input_replacement = input(f"\nWhat word would you like to replace {word} with ")
-#         if word in story_list:
-#             word = input_replacement
+for word in story_list:
+    print(word, end = ' ')
+    if word in replacement_words:
+        input_replacement = input(f"\nWhat word would you like to replace {word} with ")
+        if word in story_list:
+            word = input_replacement
    
-# print(story_list)
+print(story_list)
