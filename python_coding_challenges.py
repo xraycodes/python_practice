5b452e0e6db769c7907b7e98f0ca5400ed3996a6
xraycodes <84826985+xraycodes@users.noreply.github.com>
2023-10-10 13:21:46 -0400

    Update python_coding_challenges.py

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -26,3 +26,23 @@ def mid(string):
     else:
         half = len(string) // 2
         return string[half]
+
+################
+# Online status
+# The aim of this challenge is, given a dictionary of people's online status, to count the number of people who are online.
+# For example, consider the following dictionary:
+# statuses = {
+#     "Alice": "online",
+#     "Bob": "offline",
+#     "Eve": "online",
+# }
+# In this case, the number of people online is 2.
+# Write a function named online_count that takes one parameter. The parameter is a dictionary that maps from strings of names to the string "online" or "offline", as seen above.
+# Your function should return the number of people who are online.
+
+def online_count(dictionary):
+    count = 0
+    for values in dictionary.values():
+        if values == 'online':
+            count += 1
+    return count
