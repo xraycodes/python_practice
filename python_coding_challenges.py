4b698f7c68c8a0963e32aaeadb064386a2cbf5cf
xraycodes <84826985+xraycodes@users.noreply.github.com>
2023-10-10 13:17:30 -0400

    Update python_coding_challenges.py

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -15,3 +15,14 @@ capital_indexes("hello")
 
 
 ################
+# Middle letter
+# Write a function named mid that takes a string as its parameter. Your function should extract and return the middle letter.
+#If there is no middle letter, your function should return the empty string.
+# For example, mid("abc") should return "b" and mid("aaaa") should return "".
+
+def mid(string):
+    if len(string) % 2 == 0:
+        return ""
+    else:
+        half = len(string) // 2
+        return string[half]
