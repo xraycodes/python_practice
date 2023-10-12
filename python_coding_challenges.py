00e901f05591d1e891d07f336f23cf43bc278797
xraycodes <84826985+xraycodes@users.noreply.github.com>
2023-10-12 13:35:55 -0400

    Update python_coding_challenges.py

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -46,3 +46,18 @@ def online_count(dictionary):
         if values == 'online':
             count += 1
     return count
+
+################
+# Type check
+# Write a function named only_ints that takes two parameters. Your function should return True if both parameters are integers, and False otherwise.
+
+# For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
+
+def only_ints(x,y):
+    if (isinstance(x, int) and isinstance(y,int)):
+        return True
+    else:
+        return False
+        
+print(only_ints(1,True))
+
