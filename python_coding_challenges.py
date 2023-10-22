3c92d549de2e2e9830f7ab5ac6cc1088215d6540
brianjo94 <brianjo1994@gmail.com>
2023-10-21 22:15:12 -0400

    more challenges

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -101,5 +101,21 @@ def remove_dots(str):
             word += char
     return word
 
+################
+# Counting syllables
+# Define a function named count that takes a single parameter. 
+# The parameter is a string. The string will contain a single word divided into syllables by hyphens, 
+# such as these:
 
+# "ho-tel"
+# "cat"
+# "met-a-phor"
+# "ter-min-a-tor"
+# Your function should count the number of syllables and return it.
 
+# For example, the call count("ho-tel") should return 2.
+
+def count(word):
+    new = word.split('-')
+    return len(new)
+    
