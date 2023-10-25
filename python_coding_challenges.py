6d088fddbc62272a668666ce62d5c371e3fe173f
brianjo94 <brianjo1994@gmail.com>
2023-10-24 21:18:41 -0400

    more challenges

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -118,4 +118,18 @@ def remove_dots(str):
 def count(word):
     new = word.split('-')
     return len(new)
-    
+
+################
+# Two strings are anagrams if you can make one from the other by rearranging the letters.
+
+# Write a function named is_anagram that takes two strings as its parameters. 
+# Your function should return True if the strings are anagrams, and False otherwise.
+
+# For example, the call is_anagram("typhoon", "opython") should return True 
+# while the call is_anagram("Alice", "Bob") should return False.
+
+def is_anagram(str1, str2):
+    if sorted(str1) == sorted(str2):
+        return True
+    else:
+        return False
