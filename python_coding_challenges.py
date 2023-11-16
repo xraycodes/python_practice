7f7365034a6905e57cd2606e55c731d88b42596f
brianjo94 <brianjo1994@gmail.com>
2023-11-15 23:02:53 -0500

    more q

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -159,3 +159,33 @@ def all_equal(list):
     return True
         
 print(all_equal([1,1,1]))
+
+###############
+# Define a function named triple_and that takes three parameters and returns True only if they are all True and False otherwise.
+
+def triple_and(a,b,c):
+    if a and b and c == True:
+        return True
+    else:
+        return False
+    
+###############
+
+# Write a function that takes a list of lists and flattens it into a one-dimensional list.
+
+# Name your function flatten. It should take a single parameter and return a list.
+
+# For example, calling:
+
+# flatten([[1, 2], [3, 4]])
+# Should return the list:
+
+# [1, 2, 3, 4]
+
+def flatten(n):
+    empty = []
+    for i in n:
+        empty.extend(i)
+    return empty
+
+##############
