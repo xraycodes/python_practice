237df2d85e1ff051e1ed36abaaf59d25054ce5e6
brianjo94 <brianjo1994@gmail.com>
2023-12-10 21:49:14 -0500

    more q

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -213,3 +213,36 @@ def consecutive_zeros(n):
     return max(store)
     
 print(consecutive_zeros("10110010101010"))
+
+##############
+# Check if Palindrome
+def palindrome(n):
+    if n == n[::-1]:
+        return True
+    else:
+        return False
+    
+##############
+#custom Zip
+
+# The built-in zip function "zips" two lists. Write your own implementation of this function.
+# Define a function named zap. The function takes two parameters, a and b. These are lists.
+# Your function should return a list of tuples. Each tuple should contain one item from the a list and one from b.
+# You may assume a and b have equal lengths.
+# If you don't get it, think of a zipper.
+# For example:
+# zap(
+#     [0, 1, 2, 3],
+#     [5, 6, 7, 8]
+# )
+# Should return:
+# [(0, 5),
+#  (1, 6),
+#  (2, 7),
+#  (3, 8)]
+
+def zap(a, b):
+    result = []
+    for _ in range(len(a)):
+        result.append((a[_],b[_]))
+    return result
