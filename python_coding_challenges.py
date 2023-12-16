c3be555b058f86eef5c916bfeb5a3be974da0d7a
brianjo94 <brianjo1994@gmail.com>
2023-12-15 21:43:38 -0500

    more q

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -245,3 +245,13 @@ def zap(a, b):
     for _ in range(len(a)):
         result.append((a[_],b[_]))
     return result
+
+##############
+# Up and down
+# Define a function named up_down that takes a single number as its parameter. 
+# Your function return a tuple containing two numbers; the first should be one lower than the parameter,
+#  and the second should be one higher.
+
+# For example, calling up_down(5) should return (4, 6).
+def up_down(number):
+    return (number - 1, number + 1)
