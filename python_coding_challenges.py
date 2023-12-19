733ed167c23439be2444b0045b2c576b05eac650
brianjo94 <brianjo1994@gmail.com>
2023-12-18 20:53:50 -0500

    more q

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -270,3 +270,12 @@ def largest_difference(numb):
 def up_down(number):
     return (number - 1, number + 1)
 
+##############
+# Counting parameters
+# Define a function param_count that takes a variable number of parameters. 
+# The function should return the number of arguments it was called with.
+
+# For example, param_count() should return 0, while param_count(2, 3, 4) should return 3.
+def param_count(*n):
+    return len(n)
+    
