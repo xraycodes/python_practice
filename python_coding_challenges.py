42ee8fba2bc7051b98923353d4ea344ffe7d3c7c
brianjo94 <brianjo1994@gmail.com>
2024-01-10 21:35:00 -0500

    more q

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -293,3 +293,44 @@ def list_xor(n,list1,list2):
 def param_count(*n):
     return len(n)
     
+
+##############
+
+# We have two monkeys, a and b, and the parameters a_smile 
+# and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+
+
+# monkey_trouble(True, True) → True
+# monkey_trouble(False, False) → True
+# monkey_trouble(True, False) → False
+def monkey_trouble(a_smile, b_smile):
+  if a_smile and b_smile:
+    return True
+  if not a_smile and not b_smile:
+    return True
+  return False
+
+##############
+# Given two int values, return their sum. 
+# Unless the two values are the same, then return double their sum.
+def sum_double(a, b):
+  if a == b:
+    return (a + b) * 2
+  else:
+    return a + b
+  
+  def sum_double(a, b):
+  # Store the sum in a local variable
+  sum = a + b
+  
+  # Double it if a and b are the same
+  if a == b:
+    sum = sum * 2
+  return sum
+  
+
+
+
+
+
+
