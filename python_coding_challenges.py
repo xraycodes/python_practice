af9351f0e62a8719d17b09cd0ea44f4120685c66
xraycodes <84826985+xraycodes@users.noreply.github.com>
2024-01-16 23:26:33 -0500

    Update python_coding_challenges.py
    more q

--- python_coding_challenges.py
+++ python_coding_challenges.py
@@ -327,6 +327,20 @@ def sum_double(a, b):
   if a == b:
     sum = sum * 2
   return sum
+
+
+##############
+# Give me the discount
+# Create a function in Python that accepts two parameters. 
+# The first should be the full price of an item as an integer. 
+# The second should be the discount percentage as an integer.
+
+# The function should return the price of the item after the discount has been applied. 
+# For example, if the price is 100 and the discount is 20, the function should return 80.
+def discount(n1,n2):
+    return (n1 -(n1*(n2/100)))
+    
+print(int(discount(120,55)))
   
 
 
